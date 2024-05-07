c84620a1055cdc45df866b936624289c0a807bd8
Samanvith <Samanvith2005@gmail.com>
2024-05-07 12:19:16 +0530

    changed cloudinary name

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,7 +3,7 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    cloud_name: "samanvith", 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET 
 });
